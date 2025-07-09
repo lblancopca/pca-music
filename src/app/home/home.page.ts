84a9fb881c793f6dc125e78156561602ce2d53ea
lblancopca <leonardo.blancop@pca.edu.co>
2025-07-08 20:51:36 -0500

    agregar boton de prueba (saludar) 8/07/2025

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
+  // variables, datos
+  mensaje: string = 'Hola, desde home gage!';
   constructor() {}
+
+  // funciones, métodos
+  saludar() {
+    console.log(this.mensaje);
+  }
 }
